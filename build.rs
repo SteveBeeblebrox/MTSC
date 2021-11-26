5058c60b85dd9d4da660487255f8dd78f962d21f
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-11-26 13:45:21 -0700

    License stuff and dependency changes

--- build.rs
+++ build.rs
@@ -1,20 +1,17 @@
-use std::path::Path;
-use downloader::{Download, Downloader};
+use std::io::prelude::*;
+use std::fs::File;
 use std::env;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let version = env::var_os("CARGO_PKG_VERSION").unwrap();
     
-    let mut downloader = Downloader::builder()
-        .download_folder(Path::new("src"))
-        .parallel_requests(1)
-        .retries(1)
-        .build()
-        .unwrap();
+    let response = reqwest::get(format!("https://unpkg.com/typescript@{}/lib/typescriptServices.js", version.to_string_lossy()).as_str()).await.expect(format!("Failed to download TypeScript ({})", version.to_string_lossy()).as_str());
+    let content =  response.text().await.expect(format!("Failed to download TypeScript ({})", version.to_string_lossy()).as_str());
 
-    let typescript_services = Download::new(format!("https://unpkg.com/typescript@{}/lib/typescriptServices.js", version.to_string_lossy()).as_str());
-    downloader.download(&[typescript_services]).expect(format!("Failed to download TypeScript ({})", version.to_string_lossy()).as_str());
-
+    let mut file = File::create("src/typescriptServices.js").expect(format!("Failed to download TypeScript ({})", version.to_string_lossy()).as_str());
+    file.write_all(content.as_bytes()).expect(format!("Failed to download TypeScript ({})", version.to_string_lossy()).as_str());
+    
     println!("cargo:rerun-if-env-changed=CARGO_PKG_VERSION");
     println!("cargo:rerun-if-changed=build.rs");
 }
