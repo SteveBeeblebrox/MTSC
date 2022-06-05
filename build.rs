5e825d608169f5ea4bc5b8cb4ee575a9b4c2ab1f
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2022-06-04 18:46:12 -0600

    Started work on minifier

--- build.rs
+++ build.rs
@@ -4,14 +4,21 @@ use std::env;
 
 #[tokio::main]
 async fn main() {
-    let version = env::var_os("CARGO_PKG_VERSION").unwrap();
     
-    let response = reqwest::get(format!("https://unpkg.com/typescript@{}/lib/typescriptServices.js", version.to_string_lossy()).as_str()).await.expect(format!("Failed to download TypeScript ({})", version.to_string_lossy()).as_str());
-    let content =  response.text().await.expect(format!("Failed to download TypeScript ({})", version.to_string_lossy()).as_str());
+    // TypeScript
+    download_file(&format!("https://unpkg.com/typescript@{}/lib/typescriptServices.js", env::var_os("CARGO_PKG_VERSION").unwrap().to_string_lossy()), &"src/typescriptServices.js").await;
 
-    let mut file = File::create("src/typescriptServices.js").expect(format!("Failed to download TypeScript ({})", version.to_string_lossy()).as_str());
-    file.write_all(content.as_bytes()).expect(format!("Failed to download TypeScript ({})", version.to_string_lossy()).as_str());
-    
+    // Terser
+    download_file(&format!("https://unpkg.com/terser@{}/dist/bundle.min.js", "5.14.0"), &"src/terser.js").await;
+
     println!("cargo:rerun-if-env-changed=CARGO_PKG_VERSION");
     println!("cargo:rerun-if-changed=build.rs");
+}
+
+async fn download_file(url: &str, path: &str) {
+    let response = reqwest::get(url).await.expect(format!("Failed to download {}", url).as_str());
+    let content =  response.text().await.expect(format!("Failed to download {}", url).as_str());
+
+    let mut file = File::create(path).expect(format!("Failed to save {} to {}", url, path).as_str());
+    file.write_all(content.as_bytes()).expect(format!("Failed to save {} to {}", url, path).as_str());
 }
