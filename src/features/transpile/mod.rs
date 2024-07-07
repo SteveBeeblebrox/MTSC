c84c9b4746a74bf7b30182838bdd3d36033e47de
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2024-07-07 14:34:24 -0600

    Update

--- src/features/transpile/mod.rs
+++ src/features/transpile/mod.rs
@@ -10,109 +10,6 @@ use std::sync::{Mutex,Once,Arc};
 use std::cell::RefCell;
 use std::rc::Rc;
 
-
-/*
-thread_local! {
-    static V8_ISOLATE: Rc<RefCell<Option<v8::OwnedIsolate>>> = Rc::new(RefCell::new(None));
-    static V8_CONTEXT: Rc<RefCell<Option<v8::Local<'static, v8::Object>>>> = Rc::new(RefCell::new(None));
-}
-
-thread_local! {
-    static ISOLATE: RefCell<Option<v8::OwnedIsolate>> = RefCell::new(None);
-    static HANDLE: RefCell<Option<v8::HandleScope<'static, ()>>> = RefCell::new(None);
-}
-
-#[allow(unused)]
-fn init_ts() {
-    static TS_INIT: Once = Once::new();
-    TS_INIT.call_once(|| {
-        common::init_v8();
-
-        ISOLATE.with_borrow_mut(move |isolate| {
-            *isolate = Some(v8::Isolate::new(Default::default()));
-
-            static isolate = isolate.as_mut().unwrap();
-
-            HANDLE.with_borrow_mut(move |handle| {
-                *handle = Some(v8::HandleScope::new(isolate));
-            });
-
-            // let isolate = isolate.as_mut().unwrap();
-
-            // let scope = &mut v8::HandleScope::new(&mut *isolate);
-            // let context = v8::Context::new(scope);
-            // let scope = &mut v8::ContextScope::new(scope, context);
-
-            // let script = v8::String::new(scope, TYPESCRIPT).expect("Error with TS").into();
-            // let script = v8::Script::compile(scope, script, None).expect("Error with TS");
-            // script.run(scope).expect("Error with TS");
-        });
-
-    });
-
-
-fn get_isolate() -> Rc<RefCell<Option<v8::OwnedIsolate>>> {
-    V8_ISOLATE.with(|isolate| {
-        if isolate.borrow().is_none() {
-            common::init_v8();
-            let isolate_ref = v8::Isolate::new(Default::default());
-            isolate.replace(Some(isolate_ref));
-        }
-        Rc::clone(isolate)
-    })
-}
-
-
-// thread_local! {
-//     static ISOLATE: RefCell<v8::OwnedIsolate> = RefCell::new(v8::Isolate::new(Default::default()));
-// }
-// thread_local! {
-//     static HANDLE: v8::HandleScope<'static,()> = v8::HandleScope::new(&mut ISOLATE.get());
-// }
-
-// #[allow(unused)]
-// fn init_ts() -> &'static TS {
-//     common::init_v8();
-
-//     static ONCE: OnceLock<TS> = OnceLock::new();
-//     return ONCE.get_or_init(|| {
-//         common::init_v8();
-
-//         static isolate = v8::Isolate::new(Default::default());
-
-//         let scope = &mut v8::HandleScope::new(&mut isolate);
-//         let context = v8::Context::new(scope);
-//         let scope = &mut v8::ContextScope::new(scope, context);
-
-//         let script = v8::String::new(scope, TYPESCRIPT).expect("Error with TS").into();
-//         let script = v8::Script::compile(scope, script, None).expect("Error with TS");
-//         script.run(scope).expect("Error with TS");
-
-//         return TS {
-//             scope
-//         };
-//     });
-// }
-
-// TODO load snapshot?
-
-
-fn get_context() -> Rc<RefCell<Option<v8::Local<'static, v8::Object>>>> {
-    V8_CONTEXT.with_borrow_mut(|context| {
-        if *context.is_none() {
-            let isolate: &'static mut v8::OwnedIsolate = get_isolate().borrow_mut().as_mut().unwrap();
-            let handle = &mut v8::HandleScope::new(&mut *isolate);
-            let xcontext = v8::Context::new(handle);
-            let scope = &mut v8::ContextScope::new(handle, xcontext);
-
-
-            context.replace(Some(xcontext.global(scope)));
-        }
-        Rc::clone(context)
-    })
-}
-}*/
-
 macro_rules! timer {
     ($msg:literal,$expression:expr) => {
         {
@@ -141,15 +38,13 @@ mod tests {
 
 static TS_INIT: Once = Once::new();
 
-struct Runtime<'a,'b> {
+// Based on https://github.com/Valerioageno/ssr-rs/blob/main/src/ssr.rs
+struct Runtime<'s,'i> {
     isolate: *mut v8::OwnedIsolate,
-    handle: *mut v8::HandleScope<'a,()>,
-    context: *mut v8::Local<'a, v8::Context>,
-    scope: *mut v8::ContextScope<'b, v8::HandleScope<'a,v8::Context>>,
+    handle: *mut v8::HandleScope<'s,()>,
+    context: *mut v8::Local<'s, v8::Context>,
+    scope: *mut v8::ContextScope<'i, v8::HandleScope<'s,v8::Context>>,
 }
-
-// TODO impl drop
-// https://github.dev/Valerioageno/ssr-rs
 
 impl Drop for Runtime<'_, '_> {
     fn drop(&mut self) {
@@ -163,37 +58,46 @@ impl Drop for Runtime<'_, '_> {
     }
 }
 
+#[allow(unused)]
 impl<'s, 'i> Runtime<'s, 'i> where 's: 'i, {
     fn new() -> Self {
         common::init_v8();
 
         let isolate = Box::into_raw(Box::new(v8::Isolate::new(v8::CreateParams::default())));
+        let handle = unsafe { Box::into_raw(Box::new(v8::HandleScope::new(&mut *isolate))) };
+        let context = unsafe { Box::into_raw(Box::new(v8::Context::new(&mut *handle))) };
+        let scope = unsafe { Box::into_raw(Box::new(v8::ContextScope::new(&mut *handle, *context))) };
 
-        let handle_scope = unsafe { Box::into_raw(Box::new(v8::HandleScope::new(&mut *isolate))) };
+        return Runtime {isolate, handle, context, scope};
+    }
 
-        let context = unsafe { Box::into_raw(Box::new(v8::Context::new(&mut *handle_scope))) };
+    fn get_isolate(&self) -> &mut v8::OwnedIsolate {
+        return unsafe {&mut *self.isolate};
+    }
 
-        let scope =
-            unsafe { Box::into_raw(Box::new(v8::ContextScope::new(&mut *handle_scope, *context))) };
+    fn get_handle(&self) -> &mut v8::HandleScope<'s,()> {
+        return unsafe {&mut *self.handle};
+    }
 
-        return Runtime {isolate, handle: handle_scope, context, scope};
+    fn get_context(&self) -> &mut v8::Local<'s, v8::Context> {
+        return unsafe {&mut *self.context};
+    }
+
+    fn get_scope(&self) -> &mut v8::ContextScope<'i, v8::HandleScope<'s,v8::Context>> {
+        return unsafe {&mut *self.scope};
     }
 }
 
 thread_local! {
-    static RUNTIME: RefCell<Runtime<'static,'static>> = RefCell::new(Runtime::new());
-}
-
-thread_local! {
-    static ISOLATE: RefCell<v8::OwnedIsolate> = RefCell::new(v8::Isolate::new(Default::default()));
+    static RUNTIME: Runtime<'static,'static> = Runtime::new();
 }
 
 pub fn transpile(text: String, options: &Options) -> Option<String> {
     common::init_v8();
     
-    return RUNTIME.with_borrow_mut(|runtime| {
-        let context = unsafe {&mut *runtime.context};
-        let scope = unsafe {&mut *runtime.scope};
+    return RUNTIME.with(|runtime| {
+        let context = runtime.get_context();
+        let scope = runtime.get_scope();
         // let scope = &mut v8::HandleScope::new(isolate);
         // let context = v8::Context::new(scope);
         // let scope = &mut v8::ContextScope::new(scope, context);
